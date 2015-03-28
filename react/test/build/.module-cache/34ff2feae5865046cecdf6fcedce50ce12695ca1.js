5f180de92f057e4bee87f33e44534ee221e6ac78
Aaron <401545553@qq.com>
2015-03-28 21:53:56 +0800

    test study

---
+++ react/test/build/.module-cache/34ff2feae5865046cecdf6fcedce50ce12695ca1.js
@@ -0,0 +1,60 @@
+
+var CommentBox = React.createClass({displayName: 'CommentBox',
+  render: function() {
+    return (
+      React.createElement('div', {className: "commentBox"},
+        "Hello, world! I am a CommentBox."
+      )
+    );
+  }
+});
+React.render(
+  React.createElement(CommentBox, null),
+  document.getElementById('content')
+);
+
+
+// var TodoList = React.createClass({
+//   render: function() {
+//     var createItem = function(itemText) {
+//       return <li>{itemText}</li>;
+//     };
+//     return <ul>{this.props.items.map(createItem)}</ul>;
+//   }
+// });
+
+// var TodoApp = React.createClass({
+//   getInitialState: function() {
+//     return {items: [], text: ''};
+//   },
+//   onChange: function(e) {
+//     this.setState({text: e.target.value});
+//   },
+
+//   componentDidMount:function(){
+//   	console.log(1211111)
+//   },
+
+//   handleSubmit: function(e) {
+//     e.preventDefault();
+//     var nextItems = this.state.items.concat([this.state.text]);
+//     var nextText = '';
+//     this.setState({items: nextItems, text: nextText});
+//   },
+//   render: function() {
+//     return (
+//       <div>
+//         <h3>TODO</h3>
+//         <TodoList items={this.state.items} />
+
+//         <form onSubmit={this.handleSubmit}>
+//           <input onChange={this.onChange} value={this.state.text} />
+//           <button>{'Add #' + (this.state.items.length + 1)}</button>
+//         </form>
+        
+//       </div>
+//     );
+//   }
+// });
+
+// React.render(<TodoApp />, document.body);
