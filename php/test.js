8d62ccd08be8cfb0e425ad99c1df4cfae9757a3c
jsAaron <401545553@qq.com>
2015-04-01 21:16:38 +0800

    test php

---
+++ php/test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2015/4/1.
+ */
+
+$.ajax({
+    url      : 'test.php',
+    //dataType : 'json',
+    data: {
+        xxtsql: 'select * form seeting'
+    },
+    success:function(rs){
+        console.log(arguments)
+    },
+    error:function(){}
+})
---
+++ php/test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/4/1
+ * Time: 21:01
+ */
+ phpinfo();
