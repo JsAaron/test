9c3c521c3f8a9158d660c3196147d2219d464e8d
jsAaron <401545553@qq.com>
2015-07-15 15:39:36 +0800

    111

--- css3/七夕主题/js/Qixi.js
+++ css3/七夕主题/js/Qixi.js
@@ -16,9 +16,9 @@ var Qixi = function() {
 
     //时间设置(时间毫秒）
     var setTime = {
-        walkToThird  : 4000, //走第一段路，1/3屏幕宽度所用的时间，走完毕背景动
-        walkToMiddle : 5500, //走第二段路，1/2屏幕宽度所用的时间，走到商店
-        walkToEnd    : 5000, //走第三段路，走到桥
+        walkToThird  : 6000, //走第一段路，1/3屏幕宽度所用的时间，走完毕背景动
+        walkToMiddle : 6500, //走第二段路，1/2屏幕宽度所用的时间，走到商店
+        walkToEnd    : 6500, //走第三段路，走到桥
         
         walkTobridge : 2000, //上桥
         bridgeWalk   : 2000, //桥上走路到中间
@@ -26,11 +26,11 @@ var Qixi = function() {
         walkToShop   : 1000, //进商店时间
         walkOutShop  : 1000, //出商店时间
         
-        openDoorTime : 500, //开门时间
+        openDoorTime : 800, //开门时间
         shutDoorTime : 500, //关门时间
         
         waitRotate   : 850,//男女等待转身的时间
-        waitFlower   : 500 //模拟取花的等待时间
+        waitFlower   : 800 //模拟取花的等待时间
     }
 
     /////////
