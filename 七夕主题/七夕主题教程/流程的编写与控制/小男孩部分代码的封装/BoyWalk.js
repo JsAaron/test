39698975884beec12826593c8cf8448343aff308
jsAaron <401545553@qq.com>
2015-07-23 15:04:57 +0800

    小男孩部分代码的封装

--- 七夕主题/七夕主题教程/流程的编写与控制/小男孩部分代码的封装/BoyWalk.js
+++ 七夕主题/七夕主题教程/流程的编写与控制/小男孩部分代码的封装/BoyWalk.js
@@ -1,11 +1,14 @@
-$(function() {
+/**
+ * 小孩走路
+ * @param {[type]} container [description]
+ */
+function BoyWalk() {
 
     var container = $("#content");
     //页面可视区域
     var visualWidth = container.width()
     var visualHeight = container.height()
 
-    var swipe = Swipe(container);
     //获取数据
     var getValue = function(className) {
             var $elem = $('' + className + '')
@@ -28,9 +31,18 @@ $(function() {
     })
 
 
-    ////////////////////////////////////////////////////////
-    //===================动画处理============================ //
-    ////////////////////////////////////////////////////////
+    var boyWidth = $boy.width();
+    var boyHeight = $boy.height();
+
+    //设置下高度    
+    $boy.css({
+        top: pathY - boyHeight + 25
+    })
+
+    //暂停走路
+    function pauseWalk() {
+        $boy.addClass('pauseWalk')
+    }
 
     //恢复走路
     function restoreWalk() {
@@ -42,13 +54,6 @@ $(function() {
         $boy.addClass('slowWalk')
     }
 
-    //计算移动距离
-    function calculateDist(direction, proportion) {
-        return (direction == "x" ?
-            visualWidth : visualHeight) * proportion;
-    }
-
-
     //用transition做运动
     function stratRun(options, runTime) {
         var dfdPlay = $.Deferred();
@@ -59,10 +64,11 @@ $(function() {
             options,
             runTime,
             'linear',
-            function() {});
+            function() {
+                dfdPlay.resolve(); //动画完成
+            });
         return dfdPlay;
     }
-
 
     //开始走路
     function walkRun(time, dist, disY) {
@@ -71,18 +77,31 @@ $(function() {
         slowWalk();
         //开始走路
         var d1 = stratRun({
-            'left' : dist + 'px',
-            'top'  : disY ? disY : undefined
+            'left': dist + 'px',
+            'top': disY ? disY : undefined
         }, time);
         return d1;
     }
 
-    //开始
-    $("button:first").click(function() {
-        //10秒钟 ，走到0.5 也就是页面中间位置
-        var distX = calculateDist('x', 0.5)
-        var distY = calculateDist('y', 0.5)
-        walkRun(10000, distX, distY)
-    })
+    //计算移动距离
+    function calculateDist(direction, proportion) {
+        return (direction == "x" ?
+            visualWidth : visualHeight) * proportion;
+    }
 
-})
+    return {
+        //开始走路
+        walkTo: function(time, proportionX, proportionY) {
+            var distX = calculateDist('x', proportionX)
+            var distY = calculateDist('y', proportionY)
+            return walkRun(time, distX, distY);
+        },
+        //停止走路
+        stopWalk: function() {
+            pauseWalk();
+        },
+        setColoer:function(value){
+        	$boy.css('background-color',value)
+        }
+    }
+}
